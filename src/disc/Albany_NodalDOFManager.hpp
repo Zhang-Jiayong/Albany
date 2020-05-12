c2a1dbbbc148895be1d7dfdcba9363c8907e7830
Luca Bertagna <lbertag@sandia.gov>
2020-05-11 18:07:38 -0600

    Cleanup and fixes in the STK discretization classes.

--- src/disc/Albany_NodalDOFManager.hpp
+++ src/disc/Albany_NodalDOFManager.hpp
@@ -8,32 +8,57 @@
 #define ALBANY_NODAL_DOF_MANAGER_HPP
 
 #include "Albany_ScalarOrdinalTypes.hpp"
+#include "Albany_ThyraTypes.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
+
+#include "Teuchos_RCP.hpp"
 
 namespace Albany {
 
 class NodalDOFManager {
 public:
-  NodalDOFManager() :
-    _numComponents(0), _numLocalDOF(0), _numGlobalDOF(0), _interleaved(true){};
+  NodalDOFManager ()  = default;
 
-  void setup(int numComponents, LO numLocalDOF, GO numGlobalDOF, bool interleaved=true) {
-    _numComponents = numComponents;
-    _numLocalDOF = numLocalDOF;
-    _numGlobalDOF = numGlobalDOF;
-    _interleaved = interleaved;
+  void setup (const int numComponents, const LO numLocalNodes,
+              const GO maxGlobalNodeID, const bool interleaved) {
+    m_numComponents = numComponents;
+    m_numLocalNodes = numLocalNodes;
+    m_maxGlobalNodeID = maxGlobalNodeID;
+    m_interleaved = interleaved;
   }
 
-  inline LO getLocalDOF(LO inode, int icomp) const
-    { return (_interleaved) ? inode*_numComponents + icomp : inode + _numLocalDOF*icomp; }
-  inline GO getGlobalDOF(GO node, int icomp) const
-    { return (_interleaved) ? node *_numComponents + icomp : node + _numGlobalDOF*icomp; }
-  int numComponents() const {return _numComponents;}
+  void setup (const int numComponents, const Teuchos::RCP<const Thyra_VectorSpace>& node_vs,
+              const bool interleaved) {
+    const auto indexer = createGlobalLocalIndexer(node_vs);
+    const int numLocalNodes = indexer->getNumLocalElements();
+    const int maxGlobalGID  = indexer->getMaxGlobalGID();
+    setup(numComponents,numLocalNodes,maxGlobalGID,interleaved);
+  }
+
+  inline LO getLocalDOF(LO inode, int icomp) const {
+    if (m_interleaved) {
+      return inode*m_numComponents + icomp;
+    } else {
+      return inode + m_numLocalNodes*icomp;
+    }
+  }
+  inline GO getGlobalDOF(GO node, int icomp) const {
+    if (m_interleaved) {
+      return node*m_numComponents + icomp;
+    } else {
+      return node + m_numLocalNodes*icomp;
+    }
+  }
+
+  int numComponents() const {
+    return m_numComponents;
+  }
 
 private:
-  int _numComponents;
-  LO  _numLocalDOF;
-  GO  _numGlobalDOF;
-  bool _interleaved;
+  int  m_numComponents;
+  LO   m_numLocalNodes;
+  GO   m_maxGlobalNodeID;
+  bool m_interleaved;
 };
 
 } // namespace Albany
